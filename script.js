cc00ec2bf70fbf6c9ae8f2bd38123c3bb6ba0858
silverhand <azizsaid56789@gmail.com>
2026-03-10 09:10:11 +0000

    Add particle.js integration for visual effects

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ section.classList.add("show");
 });
 
 });
+particlesJS("particles-js", {
+particles:{
+number:{value:80},
+size:{value:3},
+move:{speed:2},
+line_linked:{enable:true},
+},
+interactivity:{
+events:{
+onhover:{enable:true,mode:"repulse"}
+}
+}
+});
